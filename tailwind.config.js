720731a06f8a97a905c3e9308ad4134a1383e3f2
matteoratti <matteoratti90@gmail.com>
2023-10-09 11:04:26 +0200

    feat: setup theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        helvetica: ["helvetica", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        orange: "#FF7500",
+        "orange-light": "#FFF1E6",
+        blue: "#334155",
+        "light-blue": "#64748B",
+      },
+    },
   },
   plugins: [],
 };
